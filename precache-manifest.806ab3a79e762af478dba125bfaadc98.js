cf2dbcbf5bf10cc079612eda6a438e9ac4ae0139
Huy Truong <huy.truong@mevry.com>
2020-02-25 14:33:21 +0700

    Updates

---
+++ precache-manifest.806ab3a79e762af478dba125bfaadc98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c15304faa84d89b1caa97f5df115939d",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "8ed47047fd21cf156d24",
+    "url": "/TodoList/static/css/main.c7cb269a.chunk.css"
+  },
+  {
+    "revision": "9acc16ab4166aaa57cda",
+    "url": "/TodoList/static/js/2.c2023afb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/TodoList/static/js/2.c2023afb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ed47047fd21cf156d24",
+    "url": "/TodoList/static/js/main.d82a2146.chunk.js"
+  },
+  {
+    "revision": "a5397dc4e169fecabd43",
+    "url": "/TodoList/static/js/runtime-main.05d6ffb9.js"
+  }
+]);
